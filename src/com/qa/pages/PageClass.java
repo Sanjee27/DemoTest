faa46c9ddf099ba17c62dce7e7172fdbaed14274
baisaliinnoraft <baisali.gupta@innoraft.com>
2019-08-26 14:40:28 +0530

    First git commit

---
+++ src/com/qa/pages/PageClass.java
@@ -0,0 +1,17 @@
+package com.qa.pages;
+
+public class PageClass {
+	
+	String msg="Git is the best version control tool";
+	
+	public void printMsg(){
+		
+		System.out.println(msg);
+	
+				
+	}
+	
+
+	}
+
+
---
+++ src/com/qa/test/Test.java
@@ -0,0 +1,16 @@
+package com.qa.test;
+
+import com.qa.pages.PageClass;
+
+public class Test extends PageClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Test test=new Test();
+		
+		test.printMsg();
+
+	}
+
+}
